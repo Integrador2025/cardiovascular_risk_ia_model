8149a492e5fd32f4887de6003d101a1745079ada
Bryan <bryan.1701910677@ucaldas.edu.co>
2025-03-25 13:27:47 -0500

    first commit

---
+++ load.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def load_dataset():
+    # Cargar dataset de pacientes
+    df_pacientes = pd.read_csv("datasets/pacient_dataset.csv")
+
+    # Cargar dataset de códigos municipios y departamentos
+    df_codigos = pd.read_csv("datasets/codigo_departamentos_dataset.csv")
+
+    # Cargar dataset de información geográfica
+    df_departamentos = pd.read_csv("datasets/departamentos_dataset.csv")
+
+    return df_pacientes, df_codigos, df_departamentos
